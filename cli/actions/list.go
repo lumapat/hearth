d1a4f43dea46fcf015b3c20d49a36692c2f75888
Miguel Lumapat <mlumapat@bloomberg.net>
2020-05-17 11:44:28 -0400

    Add roadmaps for other two functions

--- cli/actions/list.go
+++ cli/actions/list.go
@@ -6,6 +6,21 @@ import (
 	cobra "github.com/spf13/cobra"
 )
 
+// Roadmap:
+// Create a centralized YAML to store this (for now)
+//	- Not sure if we need a more persistent store but this should be enough
+// If the YAML already exists, use it
+// YAML should have the following information
+//	- List of hearths to keep burning!
+//		- Root folder location
+//		- Device name
+//		- Type of storage (e.g. USB, SSD, HDD)
+//
+// YAML will eventually have more information, and the information may
+// need to be de-centralized for better robustness. But good to defer this.
+
+// For now, just list all the storage devices on the system!
+
 func NewListCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "list",
--- cli/actions/sync.go
+++ cli/actions/sync.go
@@ -6,6 +6,23 @@ import (
 	cobra "github.com/spf13/cobra"
 )
 
+// TODO: Implement a sync
+//
+// Might need to share the code from Compare
+// but this command will need to sync files based on a
+// list of strategies
+//	- TrueMasterCopy (Highest priority ID)
+//		- Delete data not found in MasterCopy
+//		- Add only new data found only in MasterCopy
+//	- NewFilesOnly
+//		- Add only new data found in any copy across all copies
+//  - AddByMasterCopy
+//		- Add only new data found only in MasterCopy
+//		- Keep data existing in other copies
+//  - TrimByMasterCopy
+//		- Delete data not found in MasterCopy
+//		- Do not add other data
+
 func NewSyncCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "sync",
